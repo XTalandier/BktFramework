53a97d39122458c5aa595f9cc586c7c6fdcc4ed8
Xavier TALANDIER <xavier@talandier.fr>
2012-11-01 18:12:32 +0100

    fix bug : Custom tags correct reg exp and no value for password

--- Bkt/Tags.php
+++ Bkt/Tags.php
@@ -14,16 +14,16 @@ class Bkt_Tags {
 	 * @return string Parsed data
 	 */
 	public static function parse($data){
-		preg_match_all('/<bkt:([a-zA-Z]*):([a-zA-Z]*)(.*)\/>/i', $data, $result);
+		preg_match_all('/<bkt:([a-zA-Z]*):([a-zA-Z0-9]*)([^>]*)\/>/im', $data, $result);
 		if(!isset($result[0][0])){
 			return $data;
 		}
 		$n = count($result[0]);
 		for($i = 0 ; $i < $n ; $i++){
-			$type   = $result[1][$i];
+			$type   = strtolower($result[1][$i]);
 			$name   = $result[2][$i];
 			$params = $result[3][$i];
-			$value  = isset($_POST[$name]) ? $_POST[$name] : '';
+			$value  = $type == 'text' ? (isset($_POST[$name]) ? $_POST[$name] : '') : '';
 			$tag = sprintf('<input type="%s" name="%s" id="%s" %s value="%s" />' , $type , $name , $name , $params , $value);
 			$data = str_replace($result[0][$i], $tag, $data);
 		}
